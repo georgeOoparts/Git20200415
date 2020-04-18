462437b7c26067308a278c9b8a0e21a751956592
georgeOrparts <urataffy@gmail.com>
2020-04-18 21:46:07 +0900

    rrpanelをタップすると　デバックログが出る。　rrpanelとバックの同時押し無し確認。

--- Game/Pro/H_99_55_rrPanelButton.cs
+++ Game/Pro/H_99_55_rrPanelButton.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.UI;
 
-public class H_99_54_BackButton : MonoBehaviour
+public class H_99_55_rrPanelButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,11 +13,10 @@ public class H_99_54_BackButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("wrererer");
+        
     }
-
-    public void onClick() 
+    public void tup()
     {
-        Debug.Log("BBBBBBBBBack;;;;");
+        Debug.Log("RRRRRRRRRpanel;;;;");
     }
 }
