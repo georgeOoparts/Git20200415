9ffd6f9bcf36b343575b3a1db1af29bdcbe79200
georgeOrparts <urataffy@gmail.com>
2020-04-22 15:17:50 +0900

    56フラッグ　RRを作り　flag＞moveflagにアタッチ　とりあえずdebuglogで共通変数meidai　cameraposi　を表示

---
+++ Game/Pro/H_99_56_clickFlagToRR.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H_99_56_clickFlagToRR : MonoBehaviour
+{
+    //アタッチ＞flagpanel>moveflagpanel
+
+    public H_99_01_kyoutuHensu kyotu;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("H_99_56_clickFlagToRR>updatewwww::kyotu.meidaiHensu::kyotu.mainCameraPosi::"
+            + kyotu.meidaiHensu + "::" + kyotu.mainCameraPosi);
+
+        //Debug.Log("tesutdesuuu");
+    }
+}
