67c8bb9c764314fa41dd98a97b8f583473f08ff1
georgeOrparts <urataffy@gmail.com>
2020-04-27 15:23:01 +0900

    flag1_3_2～6とりあえずボタン紐づけ完了

--- Game/Pro/H_99_56_clickFlagToRR.cs
+++ Game/Pro/H_99_56_clickFlagToRR.cs
@@ -411,7 +411,142 @@ public class H_99_56_clickFlagToRR : MonoBehaviour
             + "kyotu.MCount::" + kyotu.MCount
             );
         }
-        
+        else if (boolClick1_3_2)
+        {
+            //tupUpとtupDownが同じ位置　かつ　タップの時間が一定以下ならば
+            if ((up == down) && (elapse <= tupTime))
+            {
+                //T0002M_kyotuHensu.meidaiNumber = 0;
+                //T0002M_kyotuHensu.cameraPosiNumber = 1;
+
+                kyotu.mainCameraPosi = 2;
+                //kyotu.meidaiHensu = 0;
+
+                kyotu.rrCount = 0;
+                kyotu.mojiSwitch = 3;
+                kyotu.MCount = 1;
+
+                mojiHonban.listReset();
+            }
+
+            boolClick1_3_2 = false;
+
+            Debug.Log("H_99_56_clickFlagToRR>updatewwww::kyotu.meidaiHensu::" + kyotu.meidaiHensu
+            + "kyotu.mainCameraPosi::" + kyotu.mainCameraPosi
+            + "kyotu.rrCount::" + kyotu.rrCount
+            + "kyotu.mojiSwitch::" + kyotu.mojiSwitch
+            + "kyotu.MCount::" + kyotu.MCount
+            );
+        }
+        else if (boolClick1_3_3)
+        {
+            //tupUpとtupDownが同じ位置　かつ　タップの時間が一定以下ならば
+            if ((up == down) && (elapse <= tupTime))
+            {
+                //T0002M_kyotuHensu.meidaiNumber = 0;
+                //T0002M_kyotuHensu.cameraPosiNumber = 1;
+
+                kyotu.mainCameraPosi = 2;
+                //kyotu.meidaiHensu = 0;
+
+                kyotu.rrCount = 0;
+                kyotu.mojiSwitch = 3;
+                kyotu.MCount = 1;
+
+                mojiHonban.listReset();
+            }
+
+            boolClick1_3_3 = false;
+
+            Debug.Log("H_99_56_clickFlagToRR>updatewwww::kyotu.meidaiHensu::" + kyotu.meidaiHensu
+            + "kyotu.mainCameraPosi::" + kyotu.mainCameraPosi
+            + "kyotu.rrCount::" + kyotu.rrCount
+            + "kyotu.mojiSwitch::" + kyotu.mojiSwitch
+            + "kyotu.MCount::" + kyotu.MCount
+            );
+        }
+        else if (boolClick1_3_4)
+        {
+            //tupUpとtupDownが同じ位置　かつ　タップの時間が一定以下ならば
+            if ((up == down) && (elapse <= tupTime))
+            {
+                //T0002M_kyotuHensu.meidaiNumber = 0;
+                //T0002M_kyotuHensu.cameraPosiNumber = 1;
+
+                kyotu.mainCameraPosi = 2;
+                //kyotu.meidaiHensu = 0;
+
+                kyotu.rrCount = 0;
+                kyotu.mojiSwitch = 3;
+                kyotu.MCount = 1;
+
+                mojiHonban.listReset();
+            }
+
+            boolClick1_3_4 = false;
+
+            Debug.Log("H_99_56_clickFlagToRR>updatewwww::kyotu.meidaiHensu::" + kyotu.meidaiHensu
+            + "kyotu.mainCameraPosi::" + kyotu.mainCameraPosi
+            + "kyotu.rrCount::" + kyotu.rrCount
+            + "kyotu.mojiSwitch::" + kyotu.mojiSwitch
+            + "kyotu.MCount::" + kyotu.MCount
+            );
+        }
+        else if (boolClick1_3_5)
+        {
+            //tupUpとtupDownが同じ位置　かつ　タップの時間が一定以下ならば
+            if ((up == down) && (elapse <= tupTime))
+            {
+                //T0002M_kyotuHensu.meidaiNumber = 0;
+                //T0002M_kyotuHensu.cameraPosiNumber = 1;
+
+                kyotu.mainCameraPosi = 2;
+                //kyotu.meidaiHensu = 0;
+
+                kyotu.rrCount = 0;
+                kyotu.mojiSwitch = 3;
+                kyotu.MCount = 1;
+
+                mojiHonban.listReset();
+            }
+
+            boolClick1_3_5 = false;
+
+            Debug.Log("H_99_56_clickFlagToRR>updatewwww::kyotu.meidaiHensu::" + kyotu.meidaiHensu
+            + "kyotu.mainCameraPosi::" + kyotu.mainCameraPosi
+            + "kyotu.rrCount::" + kyotu.rrCount
+            + "kyotu.mojiSwitch::" + kyotu.mojiSwitch
+            + "kyotu.MCount::" + kyotu.MCount
+            );
+        }
+        else if (boolClick1_3_6)
+        {
+            //tupUpとtupDownが同じ位置　かつ　タップの時間が一定以下ならば
+            if ((up == down) && (elapse <= tupTime))
+            {
+                //T0002M_kyotuHensu.meidaiNumber = 0;
+                //T0002M_kyotuHensu.cameraPosiNumber = 1;
+
+                kyotu.mainCameraPosi = 2;
+                //kyotu.meidaiHensu = 0;
+
+                kyotu.rrCount = 0;
+                kyotu.mojiSwitch = 3;
+                kyotu.MCount = 1;
+
+                mojiHonban.listReset();
+            }
+
+            boolClick1_3_6 = false;
+
+            Debug.Log("H_99_56_clickFlagToRR>updatewwww::kyotu.meidaiHensu::" + kyotu.meidaiHensu
+            + "kyotu.mainCameraPosi::" + kyotu.mainCameraPosi
+            + "kyotu.rrCount::" + kyotu.rrCount
+            + "kyotu.mojiSwitch::" + kyotu.mojiSwitch
+            + "kyotu.MCount::" + kyotu.MCount
+            );
+        }
+
         //Debug.Log("H_99_56_clickFlagToRR>updatewwww::kyotu.meidaiHensu::"+ kyotu.meidaiHensu 
         //    + "kyotu.mainCameraPosi::"+ kyotu.mainCameraPosi
         //    + "kyotu.rrCount::" + kyotu.rrCount
@@ -427,6 +562,7 @@ public class H_99_56_clickFlagToRR : MonoBehaviour
     bool boolClick1_1_4 = false;
     bool boolClick1_1_5 = false;
     bool boolClick1_1_6 = false;
+    
     bool boolClick1_2_2 = false;
     bool boolClick1_2_3 = false;
     bool boolClick1_2_4 = false;
@@ -435,6 +571,12 @@ public class H_99_56_clickFlagToRR : MonoBehaviour
     bool boolClick1_2_7 = false;
     bool boolClick1_2_8 = false;
     bool boolClick1_2_9 = false;
+
+    bool boolClick1_3_2 = false;
+    bool boolClick1_3_3 = false;
+    bool boolClick1_3_4 = false;
+    bool boolClick1_3_5 = false;
+    bool boolClick1_3_6 = false;
 
     //uiボタンを押したとき起動するメソッド
     //canvas>MokujiPanel>MoveMokujiPanel>MokujiTextPanel1_1～6（このUIたちにインスぺで設定した）>TextMokuji1_1～6
@@ -490,6 +632,27 @@ public class H_99_56_clickFlagToRR : MonoBehaviour
     {
         boolClick1_2_9 = true;
     }
-   
+    public void onClick1_3_2()
+    {
+        boolClick1_3_2 = true;
+    }
+    public void onClick1_3_3()
+    {
+        boolClick1_3_3 = true;
+    }
+    public void onClick1_3_4()
+    {
+        boolClick1_3_4 = true;
+    }
+    public void onClick1_3_5()
+    {
+        boolClick1_3_5 = true;
+    }
+    public void onClick1_3_6()
+    {
+        boolClick1_3_6 = true;
+    }
+    
+
 
 }
