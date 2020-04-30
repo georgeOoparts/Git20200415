b666f6777ed4ee5fa19f10fefa9336a29147bc3a
georgeOrparts <urataffy@gmail.com>
2020-04-30 14:56:55 +0900

    TenmetuUiTextというファイル作成　アタッチ

---
+++ Game/Pro/H_99_58_TenmetuUiText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class H_99_58_TenmetuUiText : MonoBehaviour
+{
+    //canvas＞rrpanel＞readmepanelというtextuiを点滅表示させるプログラム
+    //アタッチ　canvas＞rrpanel＞readmepanel 
+    void Start()
+    {
+        Debug.Log("TenmetuUiText");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
