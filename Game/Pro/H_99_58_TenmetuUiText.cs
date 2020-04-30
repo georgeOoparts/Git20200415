6fdd8421eef744c160b2c610b690ee1672abe091
georgeOrparts <urataffy@gmail.com>
2020-04-30 15:27:33 +0900

    とりあえずtupを点滅表示にする

--- Game/Pro/H_99_58_TenmetuUiText.cs
+++ Game/Pro/H_99_58_TenmetuUiText.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 //k7_1:Imageコンポーネントを使う
 using UnityEngine.UI;
 
+
 public class H_99_58_TenmetuUiText : MonoBehaviour
 {
+    //点滅処理のために呼び出す
+    public H_99_01B_kyotuElapse kyotuela;
+    
     //canvas＞rrpanel＞readmepanelというtextuiを点滅表示させるプログラム
     //アタッチ　canvas＞rrpanel＞readmepanel 
-    RectTransform rt;
     void Start()
     {
-        //k7_1_1:オブジェを存在するけど見えなくする。
-        this.gameObject.GetComponent<Text>().enabled = false;
         
         
-        Debug.Log("TenmetuUiText");   
+        
+        //Debug.Log("TenmetuUiText");   
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //k7_1_1:オブジェを存在するけど見えなくする。
+        this.gameObject.GetComponent<Text>().enabled = kyotuela.tenmetuOnOff;
     }
 }
