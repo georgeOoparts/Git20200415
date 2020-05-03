8d52c1db0b2eb6614156b94473499c0fcf66a367
georgeOrparts <urataffy@gmail.com>
2020-05-03 15:04:04 +0900

    共通変数ReadMePanelCount>=1でOKを表示

--- Game/Pro/H_99_59_ReadMe.cs
+++ Game/Pro/H_99_59_ReadMe.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class H_99_59_ReadMe : MonoBehaviour
 {
     //canvas＞rrpanel>readmepanelをタップした時の処理を制御するプログラム
     //canvas＞rrpanel>readmepanelにアタッチ
+    //canvas＞rrpanel>readmepanelをtupするたびにkyotu.ReadMePanelCountを１づつ増やす
+    //kyotu.ReadMePanelCountが１になった時canvas＞rrpanel>readmepanel>OkReadMePanelを表示
     public H_99_01_kyoutuHensu kyotu;
     // Start is called before the first frame update
+    public GameObject OkReadMePanel;
+
     void Start()
     {
         
@@ -16,7 +20,12 @@ public class H_99_59_ReadMe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("H_99_59_ReadMe>update>kyotu.ReadMePanelCount::" + kyotu.ReadMePanelCount); ;
+        if (kyotu.ReadMePanelCount>=1) 
+        {
+            //k7_1_1:オブジェを存在するけど見えなくする。//uitextの時
+            OkReadMePanel.GetComponent<Text>().enabled = true;
+        }
+        Debug.Log("H_99_59_ReadMe>update>kyotu.ReadMePanelCount::" + kyotu.ReadMePanelCount);
 
     }
     //0021_99_1:uiボタンを使う
@@ -25,4 +34,5 @@ public class H_99_59_ReadMe : MonoBehaviour
         kyotu.ReadMePanelCount++;
         Debug.Log("H_99_59_ReadMe>onClickReadMe>kyotu.ReadMePanelCount::" + kyotu.ReadMePanelCount);
     }
+
 }
