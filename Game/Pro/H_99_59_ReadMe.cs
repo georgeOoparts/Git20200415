71ecf3c8e83037123f052f450079c634bae207e1
georgeOrparts <urataffy@gmail.com>
2020-05-03 15:43:28 +0900

    共通変数ReadMePanelCount=2で全部ReadMePanel配下を削除

--- Game/Pro/H_99_59_ReadMe.cs
+++ Game/Pro/H_99_59_ReadMe.cs
@@ -7,8 +7,17 @@ public class H_99_59_ReadMe : MonoBehaviour
     //canvas＞rrpanel>readmepanelをタップした時の処理を制御するプログラム
     //canvas＞rrpanel>readmepanelにアタッチ
     //canvas＞rrpanel>readmepanelをtupするたびにkyotu.ReadMePanelCountを１づつ増やす
+
     //kyotu.ReadMePanelCountが１になった時canvas＞rrpanel>readmepanel>OkReadMePanelを表示
+    //>kyotu.ReadMePanelCountが2で表示を消す
+
     //canvas＞rrpanel>TupReadMePanelの点滅制御をする
+    //>kyotu.ReadMePanelCountが2で表示を消す
+
+    //canvas＞rrpanel>TextReadMePanelがkyotu.ReadMePanelCountが2で表示を消す
+    //>kyotu.ReadMePanelCountが2で表示を消す
+
+    //canvas＞rrpanel>readmepanel
     public H_99_01_kyoutuHensu kyotu;
 
     //点滅処理のために呼び出す
@@ -17,7 +26,9 @@ public class H_99_59_ReadMe : MonoBehaviour
     public GameObject OkReadMePanel;
 
     public GameObject TupReadMePanel;
-    
+
+    public GameObject TextReadMePanel;
+
     void Start()
     {
         
@@ -28,27 +39,45 @@ public class H_99_59_ReadMe : MonoBehaviour
     {
         if (kyotu.ReadMePanelCount==0) 
         {
+            //k7_1_1:オブジェを存在するけど見えなくする。//uipanelの時
+            this.gameObject.GetComponent<Image>().enabled = true;
+            
             //k7_1_1:オブジェを存在するけど見えなくする。
             TupReadMePanel.GetComponent<Text>().enabled = kyotuela.tenmetuOnOff;
 
             //k7_1_1:オブジェを存在するけど見えなくする。//uitextの時
             OkReadMePanel.GetComponent<Text>().enabled = false;
+
+            //k7_1_1:オブジェを存在するけど見えなくする。//uitextの時
+            TextReadMePanel.GetComponent<Text>().enabled = true;
         }
         else if (kyotu.ReadMePanelCount == 1) 
         {
+            //k7_1_1:オブジェを存在するけど見えなくする。//uipanelの時
+            this.gameObject.GetComponent<Image>().enabled = true;
+
             //k7_1_1:オブジェを存在するけど見えなくする。
             TupReadMePanel.GetComponent<Text>().enabled = kyotuela.tenmetuOnOff;
 
             //k7_1_1:オブジェを存在するけど見えなくする。//uitextの時
             OkReadMePanel.GetComponent<Text>().enabled = true;
+
+            //k7_1_1:オブジェを存在するけど見えなくする。//uitextの時
+            TextReadMePanel.GetComponent<Text>().enabled = true;
         }
         else 
         {
+            //k7_1_1:オブジェを存在するけど見えなくする。//uipanelの時
+            this.gameObject.GetComponent<Image>().enabled = false;
+
             //k7_1_1:オブジェを存在するけど見えなくする。
             TupReadMePanel.GetComponent<Text>().enabled = false;
 
             //k7_1_1:オブジェを存在するけど見えなくする。//uitextの時
             OkReadMePanel.GetComponent<Text>().enabled = false;
+
+            //k7_1_1:オブジェを存在するけど見えなくする。//uitextの時
+            TextReadMePanel.GetComponent<Text>().enabled = false;
         }
         
         
