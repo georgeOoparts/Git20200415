9ab8227ea9bd55e9cc3f4d2559f402312b873bd4
georgeOrparts <urataffy@gmail.com>
2020-05-05 03:55:50 +0900

    スタートリンク実装

--- Game/Pro/H_99_60_StartPanelControll.cs
+++ Game/Pro/H_99_60_StartPanelControll.cs
@@ -7,8 +7,13 @@ using TMPro;
 
 public class H_99_60_StartPanelControll : MonoBehaviour
 {
+    public H_99_01_kyoutuHensu kyotu;
+
     public H_99_01B_kyotuElapse kyotuEla;
-    public GameObject Title;
+    
+    //public GameObject Title;
+
+    //public GameObject StartButton;
     void Start()
     {
         //k7 textmeshpro を使う場合　直接enableを記述しなければならない
@@ -19,8 +24,13 @@ public class H_99_60_StartPanelControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Title.GetComponent<TextMeshProUGUI>().enabled = kyotuEla.tenmetuOnOff;//ok
+        //Title.GetComponent<TextMeshProUGUI>().enabled = kyotuEla.tenmetuOnOff;//ok
 
-        Debug.Log("H_99_60_StartPanelControll>Title:::"+Title.name);
+        //Debug.Log("H_99_60_StartPanelControll>Title:::"+Title.name);
+    }
+    public void onClickStartButton() 
+    {
+        Debug.Log("H_99_60_StartPanelControll>Title:::");
+        kyotu.mainCameraPosi = 0;
     }
 }
