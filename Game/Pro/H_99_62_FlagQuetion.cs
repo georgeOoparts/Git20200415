060fe5ac8fc914a8b6828c1cbc5fc92e41ae651c
georgeOrparts <urataffy@gmail.com>
2020-05-10 14:27:10 +0900

    canvas>flag>moveFlag>H_99_62_FlagQuetionを作る debuglog表示する。14:10

--- Game/Pro/H_99_62_FlagQuetion.cs
+++ Game/Pro/H_99_62_FlagQuetion.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class H_99_62_FlagQuetion : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject BoolTeigi15;
+    public GameObject BoolKoujun1;
+    public GameObject BoolKoujun3;
+    public GameObject BoolKouri1;
+    public GameObject BoolShoumei1;
+
     void Start()
     {
-        Debug.Log("rererererer");
+        Debug.Log("rerer"+ BoolTeigi15.name);
     }
 
     // Update is called once per frame
