adb283983aba6479e1cd681e327c23908000abfc
georgeOrparts <urataffy@gmail.com>
2020-05-10 16:49:12 +0900

    intsave実装16;50

--- Game/Pro/Test5.cs
+++ Game/Pro/Test5.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Test5 : MonoBehaviour
 {
     // Start is called before the first frame update
-    int SaveTest = 0;
+    public int SaveTest = 0;
     void Start()
     {
-        
+        // スコアのロード
+        SaveTest = PlayerPrefs.GetInt("SCORE", 0);
     }
 
-    // Update is called once per frame
+    // 削除時の処理
+    void OnDestroy()
+    {
+        // スコアを保存
+        PlayerPrefs.SetInt("SCORE", SaveTest);
+        PlayerPrefs.Save();
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) 
         {
             SaveTest++;
         }
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            PlayerPrefs.DeleteAll();
+            SaveTest = 0;
+        }
+
         Debug.Log("Test5>Update::SaveTest::"+ SaveTest);
     }
 }
