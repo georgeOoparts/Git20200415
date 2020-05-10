63f9a5d77731cb530bab68a602cbadff4145e62d
georgeOrparts <urataffy@gmail.com>
2020-05-10 14:53:16 +0900

    BoolKoujun1を？？？を実装14;50

--- Game/Pro/H_99_62_FlagQuetion.cs
+++ Game/Pro/H_99_62_FlagQuetion.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class H_99_62_FlagQuetion : MonoBehaviour
 {
+    public H_99_01_kyoutuHensu kyotu;
+
     public GameObject BoolTeigi15;
     public GameObject BoolKoujun1;
     public GameObject BoolKoujun3;
@@ -13,12 +15,22 @@ public class H_99_62_FlagQuetion : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("rerer"+ BoolTeigi15.name);
+        Debug.Log("rerer"+ kyotu.BoolTeigi15);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (kyotu.BoolKoujun1 == true)
+        {
+            //Debug.Log("true");
+            BoolKoujun1.GetComponent<Text>().text = "公準P-1\n任意の点から任意の点へ直線を\n引くことができる";
+            BoolKoujun1.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
+        }
+        else 
+        {
+            BoolKoujun1.GetComponent<Text>().text = "???";
+            BoolKoujun1.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+        }
     }
 }
