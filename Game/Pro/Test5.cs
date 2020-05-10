58ee9498fcaba27bb0b95af0612f0b06331ce71e
georgeOrparts <urataffy@gmail.com>
2020-05-10 17:20:54 +0900

    boolsave実装　テストはOK17:20

--- Game/Pro/Test5.cs
+++ Game/Pro/Test5.cs
@@ -5,11 +5,23 @@ using UnityEngine.UI;
 public class Test5 : MonoBehaviour
 {
     // Start is called before the first frame update
+    // int boolのsave　
+    //　boolはunityでsave設定されていなく、転がっていたプログラムを使っているためデータの削除自体ができないが
+    //　実装上は問題なし。
+    
+    
     public int SaveTest = 0;
+
+    public bool SaveBool = false;
+    const string DataKey = "saveDataKey";
     void Start()
     {
         // スコアのロード
         SaveTest = PlayerPrefs.GetInt("SCORE", 0);
+
+        //Debug.Log(H_99_63_SaveData.GetBool(DataKey, false)); // false
+        //H_99_63_SaveData.SetBool(DataKey, true);
+        SaveBool = H_99_63_SaveData.GetBool(DataKey, false);
     }
 
     // 削除時の処理
@@ -18,19 +30,23 @@ public class Test5 : MonoBehaviour
         // スコアを保存
         PlayerPrefs.SetInt("SCORE", SaveTest);
         PlayerPrefs.Save();
+
+        H_99_63_SaveData.SetBool(DataKey, SaveBool);
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) 
         {
             SaveTest++;
+            SaveBool = true;
         }
         if (Input.GetMouseButtonDown(1))
         {
             PlayerPrefs.DeleteAll();
             SaveTest = 0;
+            SaveBool = false;
         }
 
-        Debug.Log("Test5>Update::SaveTest::"+ SaveTest);
+        Debug.Log("Test5>Update::SaveBool::"+ SaveBool);
     }
 }
