6fbd1818e9a6475c854bf33860fab74cea92d031
georgeOrparts <urataffy@gmail.com>
2020-05-21 14:42:39 +0900

    H_99_60_StartPanelControll　リセット 変数一応リセット　14:40 バグ継続＞定義15終わり???クリックできる

--- Game/Pro/H_99_60_StartPanelControll.cs
+++ Game/Pro/H_99_60_StartPanelControll.cs
@@ -49,33 +49,68 @@ public class H_99_60_StartPanelControll : MonoBehaviour
 
             kyotu.ReadMePanelCount = 0;
 
-            //meidai1???表示reset
+            //meidaiの内容false？？？true内容表示
+            kyotu.BoolMeidai1_1 = true;//一番最初常にtrue
             kyotu.BoolMeidai1_2 = false;
             kyotu.BoolMeidai1_3 = false;
             kyotu.BoolMeidai1_4 = false;
             kyotu.BoolMeidai1_5 = false;
             kyotu.BoolMeidai1_6 = false;
 
-            
-
-            kyotu.BoolTeigi15 = true;
-            kyotu.BoolKoujun1 = false;
-            kyotu.BoolKoujun3 = false;
-            kyotu.BoolKouri1 = false;
+            //フラグ図で一番↓meidaiの証明　内容false？？？true内容表示 フラグ↓
             kyotu.BoolShomeiMeidai1_1 = false;
+            kyotu.BoolShomeiMeidai1_2 = false;
+            kyotu.BoolShomeiMeidai1_3 = false;
+            kyotu.BoolShomeiMeidai1_4 = false;
+            kyotu.BoolShomeiMeidai1_5 = false;
+            kyotu.BoolShomeiMeidai1_6 = false;
 
+            //フラッグの内容　false　？？？：true　内容
+            kyotu.BoolKouri1 = false;
+            kyotu.BoolKouri3 = false;
+            kyotu.BoolKouri4 = false;
+            kyotu.BoolKouri5 = false;
 
-            kyotu.TupBoolMeidai1_1 = true;
+            kyotu.BoolKoujun1 = false;
+            kyotu.BoolKoujun2 = false;
+            kyotu.BoolKoujun3 = false;
+
+            kyotu.BoolTeigi1 = false;
+            kyotu.BoolTeigi15 = true;//一番最初常にtrue
+
+            //フラグパネルの↓証明のtupを表示させるか否かを判定するプログラム
+            kyotu.tupBoolShomeiMeidai1_1 = false;
+            kyotu.tupBoolShomeiMeidai1_2 = false;
+            kyotu.tupBoolShomeiMeidai1_3 = false;
+            kyotu.tupBoolShomeiMeidai1_4 = false;
+            kyotu.tupBoolShomeiMeidai1_5 = false;
+            kyotu.tupBoolShomeiMeidai1_6 = false;
+
+            //meidaipanel右上の点滅tupを表示させるか meidaipanelで使うtup
+            //H_99_61_MokujiQuetion、で使う
+            kyotu.TupBoolMeidai1_1 = true;//一番最初常にtrue
             kyotu.TupBoolMeidai1_2 = false;
             kyotu.TupBoolMeidai1_3 = false;
             kyotu.TupBoolMeidai1_4 = false;
             kyotu.TupBoolMeidai1_5 = false;
             kyotu.TupBoolMeidai1_6 = false;
 
-            //フラッグで？？？表示　内容表示
-            kyotu.TupTeigi15 = true;
+            //panel右上の点滅tupを表示させるか
+            //H_99_62_FlagQuetion、で使う
+            kyotu.TupKouri1 = false;
+            kyotu.TupKouri3 = false;
+            kyotu.TupKouri4 = false;
+            kyotu.TupKouri5 = false;
 
             kyotu.TupKoujun1 = false;
+            kyotu.TupKoujun2 = false;
+            kyotu.TupKoujun3 = false;
+
+            kyotu.TupTeigi1 = false;
+            kyotu.TupTeigi15 = true;//一番最初常にtrue
+
+
+            
 
             kyotu.tuduki = false;
 
