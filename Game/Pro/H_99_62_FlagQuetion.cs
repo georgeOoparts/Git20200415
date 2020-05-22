4b622fcfc80c7421fa187bc700defc4223a62cec
georgeOrparts <urataffy@gmail.com>
2020-05-22 12:54:17 +0900

    H_99_62_FlagQuetionの変数追加 理論な所にtupが出るバグ発生　12:55

--- Game/Pro/H_99_62_FlagQuetion.cs
+++ Game/Pro/H_99_62_FlagQuetion.cs
@@ -8,6 +8,7 @@ public class H_99_62_FlagQuetion : MonoBehaviour
     //アタッチ　canvas＞flagpanel＞moveflagpanel
     //kyotu.BoolKoujun1～6　trueなら内容表示、falseなら???を表示
     //2020/05/12フラッグパネルの次のtupすべき所をtup表示する
+    //2020/05/22フラッグパネルの次のtupすべき所をtup表示する　m1-6まで拡張
     public H_99_01_kyoutuHensu kyotu;
     public H_99_01B_kyotuElapse kyotuEla;
 
@@ -15,17 +16,34 @@ public class H_99_62_FlagQuetion : MonoBehaviour
     public GameObject henkaKoujun1;
     public GameObject henkaKoujun3;
     public GameObject henkaKouri1;
-    public GameObject henkaShoumei1;
+    public GameObject henkaShomei1;
 
     //k0014_2 :プレハブ（画面のobjでもOK）を使う objにはりつけ
     public GameObject TupPrefab;
 
-    private GameObject tupTeigi15;
+    private GameObject tupKouri1;
+    private GameObject tupKouri3;
+    private GameObject tupKouri4;
+    private GameObject tupKouri5;
 
     private GameObject tupKoujun1;
+    private GameObject tupKoujun2;
+    private GameObject tupKoujun3;
+
+    private GameObject tupTeigi1;
+    private GameObject tupTeigi15;
+
+    private GameObject tupShomeiM1;
+    private GameObject tupShomeiM2;
+    private GameObject tupShomeiM3;
+    private GameObject tupShomeiM4;
+    private GameObject tupShomeiM5;
+    private GameObject tupShomeiM6;
 
     void Start()
     {
+        //ここからフラグパネルm1_2のtupのためのオブジェ------------
+        //プレハブからtupTeigi15を作る。点滅するtupと表示されるGameオブジェクト
         //k0014_2_1 :プレハブを使う
         tupTeigi15 = Instantiate(TupPrefab) as GameObject;
 
@@ -35,6 +53,7 @@ public class H_99_62_FlagQuetion : MonoBehaviour
         //k0014_2_1_1: オブジェの名前を変化させる
         tupTeigi15.name = "ptupTeigi15";
 
+        //プレハブからtupKoujun1を作る。点滅するtupと表示されるGameオブジェクトーーーー
         //k0014_2_1 :プレハブを使う
         tupKoujun1 = Instantiate(TupPrefab) as GameObject;
 
@@ -43,13 +62,46 @@ public class H_99_62_FlagQuetion : MonoBehaviour
 
         //k0014_2_1_1: オブジェの名前を変化させる
         tupKoujun1.name = "ptupKoujun1";
-        //Debug.Log("rerer"+ kyotu.BoolTeigi15);
+
+        //プレハブからtupKoujun3を作る。点滅するtupと表示されるGameオブジェクトーーーーー
+        //k0014_2_1 :プレハブを使う
+        tupKoujun3 = Instantiate(TupPrefab) as GameObject;
+
+        // k0014_2_1_1 :プレハブをキャンバスの子供にする()
+        tupKoujun3.transform.SetParent(henkaKoujun3.GetComponent<Transform>(), false);
+
+        //k0014_2_1_1: オブジェの名前を変化させる
+        tupKoujun3.name = "ptupKoujun3";
+
+        //プレハブからtupKouri1を作る。点滅するtupと表示されるGameオブジェクト------------
+        //k0014_2_1 :プレハブを使う
+        tupKouri1 = Instantiate(TupPrefab) as GameObject;
+
+        // k0014_2_1_1 :プレハブをキャンバスの子供にする()
+        tupKouri1.transform.SetParent(henkaKouri1.GetComponent<Transform>(), false);
+
+        //k0014_2_1_1: オブジェの名前を変化させる
+        tupKouri1.name = "ptupKouri1";
+
+        //プレハブからtupShomeiM1を作る。点滅するtupと表示されるGameオブジェクト------------
+        //k0014_2_1 :プレハブを使う
+        tupShomeiM1 = Instantiate(TupPrefab) as GameObject;
+
+        // k0014_2_1_1 :プレハブをキャンバスの子供にする()
+        tupShomeiM1.transform.SetParent(henkaShomei1.GetComponent<Transform>(), false);
+
+        //k0014_2_1_1: オブジェの名前を変化させる
+        tupShomeiM1.name = "ptupKoujun1";
+
+        //ここからフラグパネルm1_2のtupのためのオブジェ------------
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //表示するもの???か内容か
+        ///まずはフラグm1-6まで
         if (kyotu.BoolKoujun1 == true)
         {
             //Debug.Log("true");
@@ -91,18 +143,19 @@ public class H_99_62_FlagQuetion : MonoBehaviour
         if (kyotu.BoolShomeiMeidai1_1 == true)
         {
             //Debug.Log("true");
-            henkaShoumei1.GetComponent<Text>().text = "公準P-1\n任意の点から任意の点へ直線を\n引くことができる";
-            henkaShoumei1.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
+            henkaShomei1.GetComponent<Text>().text = "公準P-1\n任意の点から任意の点へ直線を\n引くことができる";
+            henkaShomei1.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
         }
         else
         {
-            henkaShoumei1.GetComponent<Text>().text = "???";
-            henkaShoumei1.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+            henkaShomei1.GetComponent<Text>().text = "???";
+            henkaShomei1.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
         }
 
 
 
         //点滅するtupが表示されるか否か
+        //使うGameオブジェクトはスタートでプレハブから作っている
         if (kyotu.TupTeigi15==true) 
         {
             //点滅処理
