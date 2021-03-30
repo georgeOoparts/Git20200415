89cb0b3b168cf9eb692548e261e39dcff31fd7ff
georgeOrparts <urataffy@gmail.com>
2021-03-30 19:16:24 +0900

    numberとboolの表示　を作った。

--- Game/Pro/hTestKikaSaveTestBool.cs
+++ Game/Pro/hTestKikaSaveTestBool.cs
@@ -3,35 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 //k7_1:Imageコンポーネントを使う
 using UnityEngine.UI;
-//k7 textmeshpro を使う場合
-using TMPro;
 
-public class hTestKikaSaveTestNumber : MonoBehaviour
+public class hTestKikaSaveTestBool : MonoBehaviour
 {
     //k2_1:どこかに書かれている。Textというクラスを扱うための変数を作成
     Text text;
+
+    bool tf=true;
+
     // Start is called before the first frame update
-
-    int i = 0;
-
     void Start()
     {
         //k2_1_1:Textをこのオブジェクトで使うためのおまじない
         text = this.gameObject.GetComponent<Text>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         //k0003_3:一瞬左クリックダウン入力されたか出力０１
-        if (Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButtonDown(0))
         {
-            i++;
-            
+            if (tf == false)
+            {
+                tf = true;
+            }
+            else 
+            {
+                tf = false;
+            }
+
             //k2_1_1_1:text.text = "・・・ "でTEXTのないよう変更。
-            text.text = "Number:"+i;
+            text.text = "Bool:" + tf;
         }
-        
     }
 }
